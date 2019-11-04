feb2c4463ae28dff745ce0d19286770cba7e04ff
andres-martin <andresmartin266@gmail.com>
2019-11-03 21:42:58 -0500

    [add] expenses controller new and created methods added

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -3,4 +3,18 @@ class ExpensesController < ApplicationController
     @tab = :expenses
     @expenses = Expense.all
   end
+
+  def new
+    @expense = Expense.new
+  end
+
+  def create
+    @expense = Expense.create(expense_params)
+  end
+
+  private
+
+  def expense_params
+    params.require(:expense).permit(:concept, :amount, :date, :type_id, :category_id)
+  end
 end
