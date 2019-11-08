46b5ef79bcda456c8af8d2ab9515ccf85374cf93
andres-martin <andresmartin266@gmail.com>
2019-11-07 22:53:09 -0500

    [add] CRUD for expenses controller created and working, pending make it DRY

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -3,6 +3,8 @@
 ##
 # This class represents a Expense model
 class ExpensesController < ApplicationController
+  before_action :set_expense, only: [:edit, :update, :destroy]
+
   def index
     @tab = :expenses
     @expenses = Expense.all.order('date DESC')
@@ -16,7 +18,21 @@ class ExpensesController < ApplicationController
     @expense = Expense.create(expense_params)
   end
 
+  def edit; end
+
+  def update
+    @expense.update(expense_params)
+  end
+
+  def destroy
+    @expense.destroy
+  end
+
   private
+
+  def set_expense
+    @expense = Expense.find(params[:id])
+  end
 
   def expense_params
     params.require(:expense).permit(:concept, :amount,
