de81e6525985c0ce68afb3b5ced685b04c485d9e
andres-martin <andresmartin266@gmail.com>
2019-11-10 00:07:49 -0500

    [add] update and create methods working in modal

---
+++ app/assets/javascripts/expenses.js
@@ -0,0 +1,20 @@
+// add errors styles to modal
+const addErrorStyles = (name) => {
+    $(`#${name} label`).addClass('text-danger');
+    $(`#${name} input`).addClass('error');
+}
+
+// add errors to modal
+const addErrors = (arrayErrors) => {
+    const objErrors = {};
+    arrayErrors.forEach((error) => {
+        let errorArray = error.split(' ');
+        let key = errorArray[0].toLowerCase();
+        let errorMsg = errorArray.slice(1).join(" ");
+
+        objErrors[key] = objErrors[key] || errorMsg;
+
+        addErrorStyles(key);
+        $(`#${key} p`).text(`${objErrors[key]}`).show();
+    });
+};
